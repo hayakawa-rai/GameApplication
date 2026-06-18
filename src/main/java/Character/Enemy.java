cd8c6ee54433553e6d9f23fd65b5b1791ec51bfc
EMD-576 <EMD-576@EMD576>
2026-06-18 15:53:01 +0900

    Enemyクラスだよ

--- src/main/java/Character/Enemy.java
+++ src/main/java/Character/Enemy.java
@@ -2,7 +2,15 @@ package Character;
 
 public class Enemy extends Character{
 	
+	//敵の現在の状態を管理する変数(EnemyState型)
 	private EnemyState state = EnemyState.NORMAL;
+	
+	//ゲーム用の変数:1マスのサイズと、当たり判定に使うキャラクターのサイズ
+	private final double bliockSize = 28.0;
+	private final double characterSize = 24.0;
+	
+	//状態ごとの画像
+	private final Image normalImage
 	
 	
 
