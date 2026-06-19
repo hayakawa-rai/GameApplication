1fd9ba8b6066ebfa673299b649e638e888e1754e
EMD-576 <EMD-576@EMD576>
2026-06-19 13:04:53 +0900

    EnemyとEnemyStateクラスを考えたよby和田

---
+++ src/main/java/Characters/EnemyState.java
@@ -0,0 +1,8 @@
+package Characters;
+
+public enum EnemyState  {
+    SCATTER,    // 縄張りモード（自分の縄張りの四隅へ戻る）
+    CHASE,      // 追跡モード（仙石さんを追いかける）
+    FEVER,      // イジケモード（青くなってランダムに逃げる）
+    DEAD        // 死亡・巣に戻るモード（目玉だけになって巣に戻る）
+}
