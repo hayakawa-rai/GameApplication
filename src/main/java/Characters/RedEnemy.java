aad988deb8727bd920b6f50aeb7eacf52a725ded
EMD-568 <EMD-568@EMD568>
2026-06-23 16:03:34 +0900

    コミットと　green,redの縄張り修正　水野

--- src/main/java/Characters/RedEnemy.java
+++ src/main/java/Characters/RedEnemy.java
@@ -1,32 +1,34 @@
-//最短で追いかける　RedEnemy(赤)
+// 最短距離でプレイヤーを追跡する敵（赤）
 package Characters;
 /*
 import java.util.List;
-
 import javafx.scene.image.ImageView;
 
-// 仙石さんを最短距離で追いかける
 public class RedEnemy extends Enemy {
 
 	// マップ中心 エネミーハウス上 （仮座標）
 	private static final int START_COL = 13;
 	private static final int START_ROW = 11;
 
+	 // 縄張りエリアの中心（右上）（仮座標）
+	private static final int TERRITORY_COL = 24;
+	private static final int TERRITORY_ROW = 3;
+
 	public RedEnemy(ImageView imageView) {
+
 		super(imageView, START_COL * CELL_SIZE, START_ROW * CELL_SIZE, 1);
 	}
 
-	// 次に進む方向を決定
 	@Override
 	protected Direction decideNextDirection(List<Direction> validDirections, int[][] map, Sengoku player) {
 
-		// プレイヤーの現在マス
-		int targetCol = (int) ((player.getX() + CELL_SIZE / 2.0) / CELL_SIZE);
+		// プレイヤーの現在セル座標
+		int playerCol = (int) ((player.getX() + CELL_SIZE / 2.0) / CELL_SIZE);
 
-		int targetRow = (int) ((player.getY() + CELL_SIZE / 2.0) / CELL_SIZE);
+		int playerRow = (int) ((player.getY() + CELL_SIZE / 2.0) / CELL_SIZE);
 
-		// 一番近づける方向をEnemyクラスに選ばせる
-		return getClosestDirection(validDirections, targetCol, targetRow);
+		// プレイヤーへ最短で近づく方向
+		return getClosestDirection(validDirections, playerCol, playerRow);
 	}
 }
 */
