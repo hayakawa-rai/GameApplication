cb88e99970456978e747af2f6baee3db3cadf85e
EMD-571 <EMD-571@EMD571>
2026-07-10 16:54:49 +0900

    test

--- src/main/java/Characters/EnemyState.js
+++
@@ -1,7 +0,0 @@
-// 敵の状態を固定値として定義（JavaのEnumの代わり）
-export const EnemyState = Object.freeze({
-    SCATTER: "SCATTER", // 縄張りモード
-    CHASE:   "CHASE",   // 追跡モード（★これを追加！）
-    FEVER:   "FEVER",   // フィーバーモード
-    DEAD:    "DEAD"     // 死亡・巣に戻るモード
-});
